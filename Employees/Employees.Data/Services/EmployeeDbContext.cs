8cb54c4e7fa8b2c655c84e20c8834e2b127a1c72
Maja <compu@DESKTOP-F6NEHMN>
2020-01-21 06:09:56 -0800

    use EntityFramework

---
+++ Employees/Employees.Data/Services/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Employees.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Data.Services
+{
+    class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Employees/Employees.Data/Services/InMemoryEmployeeData.cs
@@ -0,0 +1,18 @@
+﻿using Employees.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Data.Services
+{
+    public class InMemoryEmployeeData : IEmployeeData
+    {
+        List<Employee> employees;
+        public IEnumerable<Employee> GetAll()
+        {
+            return employees;
+        }
+    }
+}
